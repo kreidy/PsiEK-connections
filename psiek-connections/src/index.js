71d5cbe9de840c41f55128d73132c5b179314f75
Kyle Reidy <kyle_reidy@prodigy.net>
2021-01-26 13:45:17 -0800

    initial react setup

---
+++ psiek-connections/src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+const testElement = <h1>Testing React</h1>
+
+// =============================================
+
+ReactDOM.render(testElement, document.getElementById('root'));
